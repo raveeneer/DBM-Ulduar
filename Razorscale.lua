7b79089ca021b51f4a9a0416e9548037764d373f
raveeneer <tom041193@gmail.com>
2017-04-25 00:25:52 +0200

    Razorscale: enrage timer set to 8 min.

--- Razorscale.lua
+++ Razorscale.lua
@@ -23,7 +23,7 @@ local warnDevouringFlameCast		= mod:NewAnnounce("WarnDevouringFlameCast", 2, 647
 local specWarnDevouringFlame		= mod:NewSpecialWarningMove(64733)
 local specWarnDevouringFlameCast	= mod:NewSpecialWarning("SpecWarnDevouringFlameCast")
 
-local enrageTimer					= mod:NewBerserkTimer(900)
+local enrageTimer					= mod:NewBerserkTimer(480)
 local timerDeepBreathCooldown		= mod:NewCDTimer(21, 64021)
 local timerDeepBreathCast			= mod:NewCastTimer(2.5, 64021)
 local timerTurret1					= mod:NewTimer(53, "timerTurret1", 48642)
