ce3620f74fc2a6bf7a00ab426a5a3e0911fbaab2
raveeneer <tom041193@gmail.com>
2017-07-13 00:27:28 +0200

    Auriaya: correct timer for TS (fear)

--- Auriaya.lua
+++ Auriaya.lua
@@ -36,7 +36,7 @@ local specWarnVoid 		= mod:NewSpecialWarningMove(64675)
 local enrageTimer		= mod:NewBerserkTimer(600)
 local timerDefender 	= mod:NewTimer(35, "timerDefender")
 local timerFear			= mod:NewCastTimer(64386)
-local timerNextFear 	= mod:NewNextTimer(35.5, 64386)
+local timerNextFear 	= mod:NewNextTimer(40, 64386)
 local timerNextSwarm 	= mod:NewNextTimer(36, 64396)
 local timerNextSonic 	= mod:NewNextTimer(27, 64688)
 local timerSonic		= mod:NewCastTimer(64688)
@@ -49,9 +49,9 @@ local catLives = 9
 function mod:OnCombatStart(delay)
 	catLives = 9
 	enrageTimer:Start(-delay)
-	timerNextFear:Start(40-delay)
-	timerNextSonic:Start(60-delay)
-	timerDefender:Start(60-delay)
+	timerNextFear:Start(35-delay) -- [r] 35
+	timerNextSonic:Start(60-delay) --[r] 50
+	timerDefender:Start(60-delay) -- [r] 65 
 end
 
 function mod:SPELL_CAST_START(args)
@@ -61,7 +61,7 @@ function mod:SPELL_CAST_START(args)
 		warnFear:Show()
 		timerFear:Start()
 		timerNextFear:Schedule(2)
-		warnFearSoon:Schedule(34)
+		warnFearSoon:Schedule(37)
 	elseif args:IsSpellID(64688, 64422) then --Sonic Screech
 		warnSonic:Show()
 		timerSonic:Start()
